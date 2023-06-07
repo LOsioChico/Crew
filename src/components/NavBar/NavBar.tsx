d4a3af59aec8c2211b4fc2a5c8cec6ba4252f7da
hernan97carp <hernan97carp@hotmail.com>
2023-06-07 16:39:15 -0300

    feat:buttonSearch

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,6 +6,7 @@ export const NavBar: React.FC = () => {
       <nav className='flex h-16 items-center justify-between bg-primary font-semibold text-white'>
         <div className='ml-8 flex items-center justify-center gap-5'>
           <Logo />
+
           <button className='relative pr-2'>
             Explorar
             <img
@@ -14,7 +15,19 @@ export const NavBar: React.FC = () => {
             />
           </button>
           <button>Nuestro top 10</button>
-          <img src='src/assets/search.svg' className='h-5 cursor-pointer' />
+          <div className='flex'>
+            <div className='absolute inset-y-0 left-0 flex  items-center pl-3'></div>
+            <input
+              type='search'
+              id='default-search'
+              className='w-full rounded-lg p-4 pl-4 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400'
+              placeholder='Buscar proyecto...'
+              required
+            />
+            <button type='submit' className=' px-1 py-1 text-sm  text-white '>
+              <img src='src/assets/search.svg' className='h-5 cursor-pointer' />
+            </button>
+          </div>
         </div>
         <div className='mr-8 flex items-center gap-5 '>
           <div className='cursor-pointer border-r border-gray-500 pr-5'>
