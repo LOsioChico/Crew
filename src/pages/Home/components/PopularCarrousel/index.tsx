ff96572215b2eb87783ffefe7b16ef4bcc8004f0
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-09 20:10:38 -0500

    refactor: Refactor PopularCarrousel component

--- src/pages/Home/components/PopularCarrousel/index.tsx
+++ src/pages/Home/components/PopularCarrousel/index.tsx
@@ -1,18 +1,18 @@
-import '@splidejs/splide/css/skyblue'
+import { projectExamples } from '@/data'
 import { usePopularSplide } from '../../hooks'
+import { ProjectItem } from './components'
 
 export const PopularCarrousel: React.FC = () => {
   usePopularSplide()
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <p className='mb-4 text-2xl font-bold'>Popular Projects</p>
+    <div className='flex h-full flex-col items-center justify-center py-12'>
+      <p className='self-center text-3xl font-bold'>Popular Projects</p>
       <div className='splide w-full' id='splide1' role='group'>
-        <div className='splide__track'>
+        <div className='splide__track h-[700px]'>
           <ul className='splide__list'>
-            <li className='splide__slide bg-red-400'></li>
-            <li className='splide__slide bg-gray-400'></li>
-            <li className='splide__slide bg-blue-400'></li>
-            <li className='splide__slide bg-slate-800'></li>
+            {projectExamples.map((project) => (
+              <ProjectItem key={project.id} project={project} />
+            ))}
           </ul>
         </div>
       </div>
