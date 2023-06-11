3cb717e78e25284102cf486129f82f3dda170da8
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-10 19:05:52 -0500

    feat: add main carrousel projects data

--- src/data/projectExamples.ts
+++ src/data/projectExamples.ts
@@ -1,11 +1,18 @@
 import { type IProject } from '@/interfaces'
 
-export type PopularCarrouselProjects = Omit<
+export type PopularCarrouselProject = Pick<
   IProject,
-  'images' | 'fundingGoal' | 'fundingGoalReached' | 'updates' | 'creator'
+  | 'id'
+  | 'mainImage'
+  | 'title'
+  | 'description'
+  | 'fundingCurrent'
+  | 'fundingPercentage'
+  | 'fundingDaysLeft'
+  | 'categories'
 >
 
-export const projectExamples: PopularCarrouselProjects[] = [
+export const popularCarrouselProjects: PopularCarrouselProject[] = [
   {
     id: '1',
     mainImage: 'https://picsum.photos/500/400',
@@ -49,5 +56,48 @@ export const projectExamples: PopularCarrouselProjects[] = [
     fundingPercentage: 78,
     fundingDaysLeft: 30,
     categories: ['category1', 'category2'],
+  },
+]
+
+export type MainCarrouselProject = Pick<
+  IProject,
+  'id' | 'mainImage' | 'title' | 'shortDescription'
+>
+
+export const mainCarrouselProjects: MainCarrouselProject[] = [
+  {
+    id: '102',
+    mainImage: 'https://picsum.photos/1000/500',
+    title: 'Project 102',
+    shortDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: '103',
+    mainImage: 'https://picsum.photos/1050/500',
+    title: 'Project 103',
+    shortDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: '104',
+    mainImage: 'https://picsum.photos/1100/500',
+    title: 'Project 104',
+    shortDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: '105',
+    mainImage: 'https://picsum.photos/1150/500',
+    title: 'Project 105',
+    shortDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: '106',
+    mainImage: 'https://picsum.photos/1200/500',
+    title: 'Project 106',
+    shortDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
 ]
