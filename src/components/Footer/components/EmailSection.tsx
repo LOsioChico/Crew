a89b06b87001e10d34e2be4e6fc1c65cdf81db1c
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-11 21:24:52 -0500

    style: Update EmailSection component styles

--- src/components/Footer/components/EmailSection.tsx
+++ src/components/Footer/components/EmailSection.tsx
@@ -1,7 +1,7 @@
 export const EmailSection: React.FC = () => {
   return (
     <div className='w-full max-w-xs '>
-      <span className='material-symbols-outlined mb-1 flex justify-center text-6xl text-[#252E56]'>
+      <span className='material-symbols-outlined mb-1 flex select-none justify-center text-6xl text-primary'>
         mail
       </span>
       <h3 className='text-center text-lg font-bold'>Find it first on Crew</h3>
@@ -14,15 +14,21 @@ export const EmailSection: React.FC = () => {
         className='mb-5 h-12 w-full border pl-4'
       />
 
-      <div className='flex '>
-        <input type='checkbox' className='mr-3 h-9 w-9' />
-        <p>
-          I agree to the Terms of Use and have read and understand the Privacy
-          Policy
-        </p>
+      <div className='flex'>
+        <input type='checkbox' className='mr-3 h-9 w-9' id='agree' />
+        <label htmlFor='agree'>
+          I agree to the{' '}
+          <span className='cursor-pointer underline hover:text-secondaryDark'>
+            Terms of Use
+          </span>{' '}
+          and have read and understand the{' '}
+          <span className='cursor-pointer underline hover:text-secondaryDark'>
+            Privacy Policy
+          </span>
+        </label>
       </div>
-      <button className='mt-5 h-12 w-full bg-[#252E56] font-bold text-white hover:bg-[#00146d]'>
-        SIGN ME UP
+      <button className='mt-5 h-12 w-full rounded-md bg-primary font-bold uppercase text-white duration-300 hover:bg-secondaryDark'>
+        Sign Me Up
       </button>
     </div>
   )
