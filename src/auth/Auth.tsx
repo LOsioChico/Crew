7c1fb0a54c8704be528ec096f4f3b4a227f1c465
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-12 15:37:45 -0500

    feat: add Auth component and overflow-hidden utility class

---
+++ src/auth/Auth.tsx
@@ -0,0 +1,25 @@
+import { useAuthModal } from '@/store'
+import { Login } from './components'
+
+export const Auth: React.FC = () => {
+  const body = document.querySelector('body')
+  const { state, closeAuthModal } = useAuthModal()
+
+  if (state === 'closed') {
+    body?.classList.remove('overflow-hidden')
+    return null
+  } else {
+    body?.classList.add('overflow-hidden')
+  }
+
+  return (
+    <div
+      className='absolute -top-20 z-10 flex h-[120%] w-screen items-start  justify-center overflow-scroll bg-black bg-opacity-40'
+      onClick={(e) => {
+        if (e.target === e.currentTarget) closeAuthModal()
+      }}
+    >
+      {state === 'login' && <Login />}
+    </div>
+  )
+}
