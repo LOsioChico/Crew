3072596f72f9e3bdd45bb62071f95a6d7d9c1ae8
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-14 00:02:41 -0500

    feat: Add AuthInput component to Login and Register pages

--- src/auth/components/Login.tsx
+++ src/auth/components/Login.tsx
@@ -1,9 +1,10 @@
 import { useAuthModal } from '@/store'
+import { AuthInput } from '.'
 
 export const Login: React.FC = () => {
   const { closeAuthModal, openRegisterModal } = useAuthModal()
   return (
-    <div className='mt-48 w-full max-w-md rounded-lg bg-white p-6 shadow-lg'>
+    <div className='mt-48 w-full max-w-sm rounded-lg bg-white p-6 shadow-lg'>
       <div className='flex justify-end'>
         <button
           className='right-0 top-0 text-xl duration-300 hover:text-red-700'
@@ -18,53 +19,30 @@ export const Login: React.FC = () => {
       <h2 className='mb-1 flex items-center justify-center text-3xl font-bold'>
         Welcome Back!
       </h2>
-      <p className='mb-6 flex items-center  justify-center text-gray-600'>
+      <p className='mb-4 flex items-center  justify-center text-gray-600'>
         Log in to continue
       </p>
 
-      <div className='mb-6'>
-        <div className='mb-2'>
-          <label htmlFor='email' className='block pl-1 text-sm text-gray-700'>
-            Email
-          </label>
-          <input
-            id='email'
-            type='email'
-            name='email'
-            className='h-10 w-full rounded-md border-2 border-gray-300 pl-3 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-            placeholder='Your Email'
-          />
-        </div>
-
-        <div className='mb-4'>
-          <label
-            htmlFor='password'
-            className='block pl-1 text-sm text-gray-700'
-          >
-            Password
-          </label>
-          <input
-            id='password'
-            type='password'
-            name='password'
-            className='h-10 w-full rounded-md border-2 border-gray-300 pl-3 font-light shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-            placeholder='Your Password'
-          />
-        </div>
-
-        <div className='mb-4'>
-          <a
-            href='#'
-            className='flex justify-end text-sm underline duration-300 hover:text-secondaryDark'
-          >
-            Forgot your password?
-          </a>
-        </div>
-
-        <button className='mb-2 w-full rounded-md bg-primary px-4 py-2 text-sm font-semibold text-white duration-300 hover:bg-secondaryDark'>
-          LOG IN
-        </button>
+      <div className='mb-4'>
+        <AuthInput label='Email' type='email' placeholder='Your Email' />
+        <AuthInput
+          label='Password'
+          type='password'
+          placeholder='Your Password'
+        />
       </div>
+      <div className='mb-2'>
+        <a
+          href='#'
+          className='flex justify-end text-sm underline duration-300 hover:text-secondaryDark'
+        >
+          Forgot your password?
+        </a>
+      </div>
+
+      <button className='mb-5 w-full rounded-md bg-primary px-4 py-2 text-sm font-semibold text-white duration-300 hover:bg-secondaryDark'>
+        LOG IN
+      </button>
       <p className='flex justify-center'>
         New to Crew?&nbsp;
         <span
--- src/auth/components/Register.tsx
+++ src/auth/components/Register.tsx
@@ -1,9 +1,10 @@
 import { useAuthModal } from '@/store'
+import { AuthInput } from '.'
 
 export const Register: React.FC = () => {
   const { closeAuthModal, openLoginModal } = useAuthModal()
   return (
-    <main className='mt-40 w-full max-w-md rounded-lg bg-white p-6 shadow-lg'>
+    <main className='mt-40 w-full max-w-sm rounded-lg bg-white p-6 shadow-lg'>
       <section className='flex justify-end'>
         <button
           className='right-0 top-0 text-xl duration-300 hover:text-red-700'
@@ -18,98 +19,34 @@ export const Register: React.FC = () => {
       <h2 className='mb-1 flex items-center justify-center text-3xl font-bold'>
         Welcome!
       </h2>
-      <p className='mb-6 flex items-center  justify-center text-gray-600'>
+      <p className='mb-4 flex items-center  justify-center text-gray-600'>
         Sign up to join Crew
       </p>
 
-      <section className='mb-6'>
-        <div className='mb-3'>
-          <label
-            htmlFor='text'
-            className='block text-base font-medium text-gray-700'
-          >
-            First Name
-          </label>
-          <input
-            id='First_Name'
-            type='text'
-            name='First Name'
-            className='h-10 w-full rounded-md border-2 border-gray-300 pl-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-            placeholder='Your First Name'
-          />
-        </div>
-        <div className='mb-3'>
-          <label
-            htmlFor='email'
-            className='block text-base font-medium text-gray-700'
-          >
-            Last Name
-          </label>
-          <input
-            id='Last_Name'
-            type='text'
-            name='Last Name'
-            className='h-10 w-full rounded-md border-2 border-gray-300 pl-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-            placeholder='Your Last Name'
-          />
-        </div>
-        <div className='mb-3'>
-          <label
-            htmlFor='email'
-            className='block text-base font-medium text-gray-700'
-          >
-            Email
-          </label>
-          <input
-            id='email'
-            type='email'
-            name='email'
-            className='h-10 w-full rounded-md border-2 border-gray-300 pl-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-            placeholder='Your Email'
-          />
-        </div>
+      <AuthInput label='First Name' type='text' placeholder='Your First Name' />
+      <AuthInput label='Last Name' type='text' placeholder='Your Last Name' />
+      <AuthInput label='Email' type='email' placeholder='Your Email' />
+      <AuthInput label='Password' type='password' placeholder='Your Password' />
 
-        <div className='mb-6'>
-          <label
-            htmlFor='password'
-            className='block text-base font-medium text-gray-700'
-          >
-            Password
-          </label>
-          <input
-            id='password'
-            type='password'
-            name='password'
-            className='h-10 w-full rounded-md border-2 border-gray-300 pl-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-            placeholder='Your Password'
-          />
+      <section className='flex pl-1 align-baseline text-sm'>
+        <div>
+          <input type='checkbox' className='mr-1 h-4 w-5' id='Terms' required />
         </div>
-
-        <div className='flex pb-2 pl-1 align-baseline text-sm'>
-          <input type='checkbox' className='mr-2 h-4 w-4' id='Sign' />
-          <label htmlFor='Sign'>Sign me up for the Crew newsletter</label>
-        </div>
-
-        <section className='flex pl-1 align-baseline text-sm'>
-          <div>
-            <input type='checkbox' className='mr-2 h-4 w-4' id='Terms' />
-          </div>
-          <label htmlFor='Terms'>
-            I agree to the{' '}
-            <span className='cursor-pointer font-bold underline duration-300 hover:text-secondaryDark'>
-              Terms of Use
-            </span>{' '}
-            and have read and understand the{' '}
-            <span className='cursor-pointer font-bold underline duration-300 hover:text-secondaryDark'>
-              Privacy Policy
-            </span>
-          </label>
-        </section>
-
-        <button className='mb-2 mt-6 h-10 w-full rounded-md bg-primary px-4 py-2 text-sm font-semibold text-white duration-300 hover:bg-secondaryDark'>
-          CREATE ACCOUNT
-        </button>
+        <label htmlFor='Terms'>
+          I agree to the{' '}
+          <span className='cursor-pointer font-bold underline duration-300 hover:text-secondaryDark'>
+            Terms of Use
+          </span>{' '}
+          and have read and understand the{' '}
+          <span className='cursor-pointer font-bold underline duration-300 hover:text-secondaryDark'>
+            Privacy Policy
+          </span>
+        </label>
       </section>
+
+      <button className='mb-2 mt-6 h-10 w-full rounded-md bg-primary px-4 py-2 text-sm font-semibold text-white duration-300 hover:bg-secondaryDark'>
+        CREATE ACCOUNT
+      </button>
       <p className='flex justify-center pt-1'>
         Already have an account?&nbsp;
         <span
