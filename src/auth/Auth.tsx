da16e8e6f5a6247f1c4a5cb3fde07eebfcb855d8
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-13 23:17:20 -0500

    fix: Auth modal background fixed

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -2,19 +2,12 @@ import { useAuthModal } from '@/store'
 import { Login, Register } from './components'
 
 export const Auth: React.FC = () => {
-  const body = document.querySelector('body')
   const { state, closeAuthModal } = useAuthModal()
-
-  if (state === 'closed') {
-    body?.classList.remove('overflow-hidden')
-    return null
-  } else {
-    body?.classList.add('overflow-hidden')
-  }
+  if (state === 'closed') return null
 
   return (
     <div
-      className='absolute -top-20 z-10 flex h-[120%] w-screen items-start  justify-center overflow-scroll bg-black bg-opacity-40'
+      className='fixed -top-20 z-10 flex h-[120%] w-screen items-start justify-center bg-black bg-opacity-40'
       onClick={(e) => {
         if (e.target === e.currentTarget) closeAuthModal()
       }}
