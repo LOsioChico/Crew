4d7a6ddf938cc5df26e77d6d63cc0a2331862810
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-14 09:41:21 -0500

    style: Rearrange imports and update text size and alignment

--- src/pages/Home/components/TopCategories/TopCategories.tsx
+++ src/pages/Home/components/TopCategories/TopCategories.tsx
@@ -1,22 +1,25 @@
-import logo from '../../../../assets/favicon.png'
 import tecnologia from '../../../../assets/Iconos/chipUno.png'
-import dataScience from '../../../../assets/Iconos/science.png'
-import entretenimiento from '../../../../assets/Iconos/peliculas.png'
 import health from '../../../../assets/Iconos/corazon.png'
+import entretenimiento from '../../../../assets/Iconos/peliculas.png'
+import dataScience from '../../../../assets/Iconos/science.png'
 import search from '../../../../assets/Iconos/search.png'
+import logo from '../../../../assets/favicon.png'
+
 export const TopCategories: React.FC = () => {
   return (
-    <div className=' m-auto mt-8  flex h-[250px] w-full flex-col items-center justify-around '>
-      <div className='mt-0 box-border flex h-[125px] w-full justify-center   '>
+    <div className=' m-auto mt-8 flex h-[250px] w-full flex-col items-center justify-around '>
+      <div className='mb-4 mt-0 box-border flex h-[125px] w-full justify-center   '>
         <div className='flex h-[125px] items-center justify-center '>
-          <img className='' src={logo} />
+          <img src={logo} />
         </div>
-        <div className='flex  w-4/5 flex-col items-center justify-start  p-5'>
-          <h2 className='text-lg font-bold '>Which categories interest you?</h2>
-          <h4 className='text-center text-xs'>
+        <div className='flex w-4/5 flex-col items-center justify-start p-5'>
+          <h2 className='text-2xl font-bold '>
+            Which categories interest you?
+          </h2>
+          <p className='text-center text-sm'>
             projects just for you and get great recommendations when .
-          </h4>
-          <div className='mt-3 box-border flex w-1/2  justify-center gap-3   text-xs '>
+          </p>
+          <div className='mt-3 box-border flex w-1/2  justify-center gap-3   text-sm'>
             <button className=' rounded-sm  border-secondaryDark bg-secondaryDark px-3 py-1 uppercase text-white '>
               Sign Up
             </button>
