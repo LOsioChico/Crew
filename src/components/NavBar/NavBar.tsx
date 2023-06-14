15e0f7049d3d3fee1774382507e8368bfe83c571
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-14 16:44:11 -0500

    style: Improve NavBar UI and UX

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,30 +1,38 @@
 import { Logo } from '@/assets/Logo'
 import { useAuthModal } from '@/store'
+import { useNavigate } from 'react-router-dom'
 import { SearchBar } from '.'
 
 export const NavBar: React.FC = () => {
   const { openLoginModal } = useAuthModal()
   const { openRegisterModal } = useAuthModal()
+  const navigate = useNavigate()
   return (
     <header>
       <nav className='flex h-16 items-center justify-between bg-primary font-semibold text-white'>
         <div className='ml-8 flex items-center justify-center gap-5'>
-          <Logo />
-          <button className='relative pr-2'>
+          <div
+            className='cursor-pointer duration-300'
+            onClick={() => {
+              navigate('/')
+            }}
+          >
+            <Logo />
+          </div>
+          <button className='relative pr-2 duration-300 hover:text-secondary'>
             Explorar
-            <img
-              src='/src/assets/angle-down.svg'
-              className='absolute -right-3 top-1 h-5'
-            />
+            <span className='material-symbols-outlined absolute -top-1 text-2xl'>
+              expand_more
+            </span>
           </button>
         </div>
         <SearchBar />
         <div className='mr-8 flex items-center gap-5 '>
-          <div className='cursor-pointer border-r border-gray-500 pr-5'>
+          <div className='cursor-pointer border-r border-gray-500 pr-5 duration-300 hover:text-secondary'>
             Crear una Campaña
           </div>
           <button
-            className='cursor-pointer'
+            className='cursor-pointer duration-300 hover:text-secondary'
             onClick={() => {
               openLoginModal()
             }}
@@ -32,7 +40,7 @@ export const NavBar: React.FC = () => {
             Iniciar Sesión
           </button>
           <button
-            className='cursor-pointer'
+            className='cursor-pointer duration-300 hover:text-secondary'
             onClick={() => {
               openRegisterModal()
             }}
