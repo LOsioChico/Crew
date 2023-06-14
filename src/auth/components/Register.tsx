fa3f892f37f03f4523c9d6ca234c326ea16d8821
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-13 23:39:47 -0500

    style(Register): Update styles and text labels

--- src/auth/components/Register.tsx
+++ src/auth/components/Register.tsx
@@ -6,7 +6,7 @@ export const Register: React.FC = () => {
     <main className='mt-40 w-full max-w-md rounded-lg bg-white p-6 shadow-lg'>
       <section className='flex justify-end'>
         <button
-          className='right-0 top-0'
+          className='right-0 top-0 text-xl duration-300 hover:text-red-700'
           onClick={() => {
             closeAuthModal()
           }}
@@ -15,7 +15,7 @@ export const Register: React.FC = () => {
         </button>
       </section>
 
-      <h2 className='mb-4 flex items-center justify-center text-2xl font-bold'>
+      <h2 className='mb-1 flex items-center justify-center text-3xl font-bold'>
         Welcome!
       </h2>
       <p className='mb-6 flex items-center  justify-center text-gray-600'>
@@ -85,43 +85,41 @@ export const Register: React.FC = () => {
           />
         </div>
 
-        <div className='flex pb-2 align-baseline'>
-          <input type='checkbox' className='mr-2 h-5 w-5' id='Sign' />
+        <div className='flex pb-2 pl-1 align-baseline text-sm'>
+          <input type='checkbox' className='mr-2 h-4 w-4' id='Sign' />
           <label htmlFor='Sign'>Sign me up for the Crew newsletter</label>
         </div>
 
-        <section className='flex align-baseline'>
+        <section className='flex pl-1 align-baseline text-sm'>
           <div>
-            <input type='checkbox' className='mr-2 h-5 w-5' />
+            <input type='checkbox' className='mr-2 h-4 w-4' id='Terms' />
           </div>
-          <div>
-            <label htmlFor='Terms'>
-              I agree to the{' '}
-              <span className='cursor-pointer font-bold underline hover:text-indigo-600'>
-                Terms of Use
-              </span>{' '}
-              and have read and understand the{' '}
-              <span className='cursor-pointer font-bold underline hover:text-indigo-600'>
-                Privacy Policy
-              </span>
-            </label>
-          </div>
+          <label htmlFor='Terms'>
+            I agree to the{' '}
+            <span className='cursor-pointer font-bold underline duration-300 hover:text-secondaryDark'>
+              Terms of Use
+            </span>{' '}
+            and have read and understand the{' '}
+            <span className='cursor-pointer font-bold underline duration-300 hover:text-secondaryDark'>
+              Privacy Policy
+            </span>
+          </label>
         </section>
 
-        <button className='mb-2 mt-6 h-10 w-full rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white hover:bg-indigo-700'>
+        <button className='mb-2 mt-6 h-10 w-full rounded-md bg-primary px-4 py-2 text-sm font-semibold text-white duration-300 hover:bg-secondaryDark'>
           CREATE ACCOUNT
         </button>
       </section>
-      <p className='flex justify-center pt-5'>
+      <p className='flex justify-center pt-1'>
         Already have an account?&nbsp;
-        <p
-          className='cursor-pointer font-bold underline'
+        <span
+          className='cursor-pointer font-bold underline duration-300 hover:text-secondaryDark'
           onClick={() => {
             openLoginModal()
           }}
         >
           Log In
-        </p>
+        </span>
       </p>
     </main>
   )
