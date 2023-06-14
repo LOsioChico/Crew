1f18c6f0b64e75f6db011bb64390b474f0a61219
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-14 10:06:16 -0500

    feat: Add SearchBar component to NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Logo } from '@/assets/Logo'
+import { SearchBar } from '.'
 
 export const NavBar: React.FC = () => {
   return (
@@ -15,17 +16,7 @@ export const NavBar: React.FC = () => {
             />
           </button>
           <button>Nuestro top 10</button>
-          <div className='flex'>
-            <div className='absolute inset-y-0 left-0 flex  items-center pl-3'></div>
-            <input
-              type='search'
-              className='w-full rounded-lg py-2 pl-3 text-sm outline-none duration-200 focus:ring-2 focus:ring-secondaryDark'
-              placeholder='Buscar proyecto...'
-            />
-            <button type='submit' className=' px-1 py-1 text-sm  text-white '>
-              <img src='src/assets/search.svg' className='h-5 cursor-pointer' />
-            </button>
-          </div>
+          <SearchBar />
         </div>
         <div className='mr-8 flex items-center gap-5 '>
           <div className='cursor-pointer border-r border-gray-500 pr-5'>
