da708f3415e141a92443c251a7bb28da46d8abaf
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-15 19:41:29 -0500

    style: Update form input styles

--- src/pages/ProjectForm/components/ProjectTextArea.tsx
+++ src/pages/ProjectForm/components/ProjectTextArea.tsx
@@ -18,17 +18,17 @@ export const ProjectTextArea: React.FC<ProjectTextAreaProps> = ({
 }) => {
   return (
     <div className='mb-4'>
-      <label htmlFor={name} className='mb-1 block font-bold'>
+      <label htmlFor={name} className='mb-1 block pl-1 font-bold text-gray-500'>
         {label}:
       </label>
       <textarea
         {...register(name)}
         id={name}
         placeholder={placeholder}
-        className='w-full rounded border p-2'
+        className='text-md h-20 w-full rounded-md border-2 pl-3 pt-2 outline-none focus:border-secondaryDark'
       />
       {errors[name] != null && (
-        <span className='pl-1 text-sm text-red-700'>
+        <span className='pl-1 text-sm text-red-600'>
           {errors[name]?.message ?? 'Este campo es requerido'}
         </span>
       )}
