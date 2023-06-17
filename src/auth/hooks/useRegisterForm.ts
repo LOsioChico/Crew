9abb7e7e8660e96acfa4cc9d5f1e4598e8e5bab7
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-17 00:18:54 -0500

    feat: add useRegisterForm hook for registration form

---
+++ src/auth/hooks/useRegisterForm.ts
@@ -0,0 +1,48 @@
+import { type IRegisterForm } from '@/interfaces/registerForm'
+import {
+  RegisterValidation,
+  type RegisterValidationType,
+} from '@/schemas/RegisterValidation'
+import { supabase } from '@/utils'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm, type SubmitHandler } from 'react-hook-form'
+
+export const useRegisterForm = (): IRegisterForm => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    setError,
+  } = useForm<RegisterValidationType>({
+    resolver: zodResolver(RegisterValidation),
+    mode: 'onBlur',
+  })
+
+  const onSubmit: SubmitHandler<RegisterValidationType> = async (data) => {
+    try {
+      const { error: registerError } = await supabase.auth.signUp({
+        email: data.email,
+        password: data.password,
+      })
+
+      if (registerError != null) {
+        setError('email', {
+          type: 'manual',
+          message: registerError.message,
+        })
+        throw registerError
+      }
+
+      const { error: loginError } = await supabase.auth.signInWithPassword({
+        email: data.email,
+        password: data.password,
+      })
+
+      if (loginError != null) throw loginError
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return { register, handleSubmit, onSubmit, errors, isSubmitting }
+}
