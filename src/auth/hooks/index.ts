107461e3e7ca2185b2faf0061c2f94c483383357
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-17 17:34:16 -0500

    feat: Add useAuthHandler hook to handle authentication

--- src/auth/hooks/index.ts
+++ src/auth/hooks/index.ts
@@ -1,1 +1,4 @@
+export * from './index'
+export * from './useAuthHandler'
 export * from './useLoginForm'
+export * from './useRegisterForm'
---
+++ src/auth/hooks/useAuthHandler.ts
@@ -0,0 +1,51 @@
+import { supabase } from '@/utils'
+import { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+interface IUseAuth {
+  session: boolean
+  modalAuth: 'closed' | 'login' | 'register'
+  setModalAuth: React.Dispatch<
+    React.SetStateAction<'closed' | 'login' | 'register'>
+  >
+}
+
+export const useAuthHandler = (): IUseAuth => {
+  const [session, setSession] = useState(false)
+  const [modalAuth, setModalAuth] = useState<'closed' | 'login' | 'register'>(
+    'closed'
+  )
+  const location = useLocation().pathname
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    void supabase.auth.getSession().then(({ data }) => {
+      if (data.session != null) {
+        setSession(true)
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event) => {
+      if (event === 'SIGNED_IN') {
+        setSession(true)
+        setModalAuth('closed')
+      }
+      if (event === 'SIGNED_OUT') {
+        setSession(false)
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    void supabase.auth.getSession().then(({ data: { session } }) => {
+      if (location === '/project-form' && session === null) {
+        setModalAuth('login')
+        navigate('/')
+      }
+    })
+  }, [location, navigate])
+
+  return { session, modalAuth, setModalAuth }
+}
