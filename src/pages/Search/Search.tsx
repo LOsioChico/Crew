652053ba0533a5abccd63a574a2e4df850db1d74
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-20 02:30:48 -0500

    refactor: Clean up and optimize Search page code

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,21 +1,28 @@
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { CategoriesContainer } from './components/CategoriesContainer'
+import { ProjectCard } from '@/components'
+import { useEffect, useRef, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { CategoriesContainer } from './components'
 import { useSearch } from './hooks'
 import { type ShowState } from './interface'
-import { URLParams } from './utils'
+import {
+  URLParams,
+  getCategoryQuery,
+  getSearchQuery,
+  getSortQuery,
+} from './utils'
 import { handleSortSelect } from './utils/filtersHandler'
-import { ProjectCard } from '../../components/ProjectCard'
 
 export const Search: React.FC = () => {
-  const [search, setSearch] = useState<string>('')
+  const location = useLocation()
   const [showState, setShowState] = useState<ShowState>({
-    category: 'All Projects',
-    sort: 'Trending',
+    search: getSearchQuery(location.search) ?? '',
+    category: getCategoryQuery(location.search) ?? 'All Projects',
+    sort: getSortQuery(location.search) ?? 'Trending',
   })
   const navigate = useNavigate()
-  const params = URLParams({ search, showState })
+  const params = URLParams({ showState })
   const { projects } = useSearch({ params })
+  const inputRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     navigate('/search?' + params.toString())
@@ -29,7 +36,7 @@ export const Search: React.FC = () => {
           <div className='absolute top-0 h-full w-full scale-110 rounded bg-white bg-opacity-20 drop-shadow-lg ' />
           <div className='relative '>
             <h2 className='flex items-center justify-center text-6xl font-medium'>
-              Crew Campaigns
+              Crew Projects
             </h2>
             <p className='pb-3 pt-1 text-xl'>
               Fund new and groundbreaking projects, including hits from
@@ -45,21 +52,18 @@ export const Search: React.FC = () => {
       <div className='flex flex-row'>
         <CategoriesContainer setShowState={setShowState} />
         <div className='block w-full items-center justify-center border border-gray-300'>
-          <div className='flex items-center justify-center'>
+          <div className='m-4 flex items-center justify-center'>
             <input
               type='text'
               name='search'
-              className='h-8 w-1/4 rounded-md border-2 border-neutral-100 bg-neutral-100 pl-3 text-sm text-gray-600 outline-none duration-300 focus:border-secondaryDark'
+              className='h-8 w-1/4 rounded-l-md border-2 border-neutral-100 bg-neutral-100 pl-3 text-gray-600 outline-none duration-300 focus:border-secondaryDark'
               placeholder='Search for a project'
-              value={search}
-              onChange={(event) => {
-                setSearch(event.target.value)
-              }}
-              onKeyDown={(event) => {
-                if (event.key === 'Enter') {
+              ref={inputRef}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
                   setShowState((prevState) => ({
                     ...prevState,
-                    search,
+                    search: inputRef.current?.value ?? '',
                   }))
                 }
               }}
@@ -70,7 +74,7 @@ export const Search: React.FC = () => {
               onClick={() => {
                 setShowState((prevState) => ({
                   ...prevState,
-                  search,
+                  search: inputRef.current?.value ?? '',
                 }))
               }}
             >
@@ -105,9 +109,21 @@ export const Search: React.FC = () => {
           </div>
           <div className='flex flex-col items-center justify-center pb-12 pt-6'>
             <ul className='flex w-5/6 flex-wrap items-center justify-center gap-8'>
-              {projects.projects?.map((project) => (
-                <ProjectCard key={project.id} project={project} />
-              ))}
+              {projects?.length === 0 ? (
+                <div className='flex flex-col bg-gray-200'>
+                  <div className='flex flex-col items-center justify-center'>
+                    <h1 className='text-4xl font-medium'>No Results Found</h1>
+                    <p className='text-xl'>
+                      Try adjusting your search or filter to find what you are
+                      looking for
+                    </p>
+                  </div>
+                </div>
+              ) : (
+                projects?.map((project) => {
+                  return <ProjectCard key={project.id} project={project} />
+                })
+              )}
             </ul>
           </div>
         </div>
