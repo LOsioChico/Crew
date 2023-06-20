5ab152029ae7f4fa37d06ed1e7f6e28a34c26a4d
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-20 02:29:49 -0500

    feat: Add useProjectById hook function

---
+++ src/pages/Projects/hooks/useProjectById.ts
@@ -0,0 +1,40 @@
+import { CrewApi } from '@/api'
+import { images } from '@/data'
+import { type IProject } from '@/interfaces'
+import { useQuery } from '@tanstack/react-query'
+
+interface IUseProjectById {
+  project: IProject | undefined
+  isLoading: boolean
+  error: unknown
+}
+
+export const getProjectById = async (
+  id: string | undefined
+): Promise<IProject | undefined> => {
+  if (id === undefined) return undefined
+
+  const { data } = await CrewApi.get<IProject>(
+    `/projectRoute/search/byId?id=${id}`
+  )
+
+  if ('errorMessage' in data) return undefined
+
+  const project = {
+    ...data,
+    mainImage: images[0],
+    images,
+    fundingPercentage: Math.floor(Math.random() * 100),
+  }
+
+  return project
+}
+
+export const useProjectById = (id: string | undefined): IUseProjectById => {
+  const { data, isLoading, error } = useQuery<IProject | undefined>({
+    queryKey: ['project', id],
+    queryFn: async () => await getProjectById(id),
+  })
+
+  return { project: data, isLoading, error }
+}
