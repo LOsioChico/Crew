bb425f786445d34abd20dc7628955c00c41b1d0d
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-21 18:32:46 -0500

    refactor(useProjectById): remove unused imports and update argument signature

--- src/pages/Projects/hooks/useProjectById.ts
+++ src/pages/Projects/hooks/useProjectById.ts
@@ -1,6 +1,5 @@
-import { CrewApi } from '@/api'
-import { images } from '@/data'
 import { type IProject } from '@/interfaces'
+import { getProjectById } from '@/utils'
 import { useQuery } from '@tanstack/react-query'
 
 interface IUseProjectById {
@@ -9,27 +8,7 @@ interface IUseProjectById {
   error: unknown
 }
 
-export const getProjectById = async (
-  id: string | undefined
-): Promise<IProject | undefined> => {
-  if (id === undefined) return undefined
-
-  const { data } = await CrewApi.get<IProject>(
-    `/projectRoute/search/byId?id=${id}`
-  )
-
-  if ('errorMessage' in data) return undefined
-
-  const project = {
-    ...data,
-    mainImage: images[0],
-    images,
-  }
-
-  return project
-}
-
-export const useProjectById = (id: string | undefined): IUseProjectById => {
+export const useProjectById = (id: string): IUseProjectById => {
   const { data, isLoading, error } = useQuery<IProject | undefined>({
     queryKey: ['project', id],
     queryFn: async () => await getProjectById(id),
