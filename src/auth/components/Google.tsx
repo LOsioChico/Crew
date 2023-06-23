3bf5b288e15b038e7ebfd145dbb9adfa472384bf
Juan Sebastian Morel Ruiz Diaz <juanse1990@live.com.ar>
2023-06-22 21:56:14 -0300

    fix:arreglo de renderizado

--- src/auth/components/Google.tsx
+++ src/auth/components/Google.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react"
 import {supabase} from "@/utils"
-// import { CrewApi } from '@/api'
 import googleBoton from "../../assets/Iconos/googleBtnx2.png"
-// import { type UserResponse } from "@supabase/supabase-js"
 
 export const Google: React.FC = () => {
   const [url,setUrl]= useState<string|null>(null)
@@ -21,22 +19,10 @@ export const Google: React.FC = () => {
     setUrl(url)
   })
 
-  //  const user = async () :Promise<UserResponse>=> await supabase.auth.getUser()
-  //  console.log(()=>user)
-
-  // await CrewApi.post('userRoute/register', {
-  //   id: (await supabase.auth.getUser()).data.user?.id,
-  //   email: data.email,
-  //   name: data.firstName,
-  //   lastName: data.lastName,
-  // })
-
   return (
-    <div className="flex items-center justify-center mb-1 mt-3 rounded-md bg-primary px-4 py-2 duration-300 hover:bg-secondaryDark">
-      <a href={url ?? '#'} className="">
+    <a href={url ?? "/"} className="flex items-center justify-center mb-1 mt-3 rounded-md bg-primary px-4 py-2 duration-300 hover:bg-secondaryDark">
       <img src={googleBoton} alt="Google" />
-      </a>
-    </div>
+    </a>
   )
 }
 
