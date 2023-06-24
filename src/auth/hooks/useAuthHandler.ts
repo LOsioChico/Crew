840dfdd2205db72f8c545e19ff650c597a8eb484
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-24 04:34:54 -0500

    refactor(auth): remove unused variables and return type

--- src/auth/hooks/useAuthHandler.ts
+++ src/auth/hooks/useAuthHandler.ts
@@ -1,29 +1,13 @@
 import { useUserIdStore } from '@/store'
 import { isRegistered, supabase } from '@/utils'
-import { useEffect, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
-interface IUseAuth {
-  session: boolean
-  modalAuth: 'closed' | 'login' | 'register'
-  setModalAuth: React.Dispatch<
-    React.SetStateAction<'closed' | 'login' | 'register'>
-  >
-}
-
-export const useAuthHandler = (): IUseAuth => {
-  const [session, setSession] = useState(false)
-  const [modalAuth, setModalAuth] = useState<'closed' | 'login' | 'register'>(
-    'closed'
-  )
-  const location = useLocation().pathname
-  const navigate = useNavigate()
+export const useAuthHandler = (): void => {
   const { setUserId } = useUserIdStore()
 
   useEffect(() => {
     void supabase.auth.getSession().then(({ data }) => {
       if (data.session != null) {
-        setSession(true)
         setUserId(data.session.user.id)
       }
     })
@@ -32,8 +16,6 @@ export const useAuthHandler = (): IUseAuth => {
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
       if (event === 'SIGNED_IN') {
-        setSession(true)
-        setModalAuth('closed')
         if (session?.user.app_metadata.provider?.includes('google') ?? false) {
           if (session?.user.id !== undefined)
             void isRegistered(session?.user.id)
@@ -41,20 +23,8 @@ export const useAuthHandler = (): IUseAuth => {
         if (session != null) setUserId(session.user.id)
       }
       if (event === 'SIGNED_OUT') {
-        setSession(false)
         setUserId('')
       }
     })
   }, [setUserId])
-
-  useEffect(() => {
-    void supabase.auth.getSession().then(({ data: { session } }) => {
-      if (location === '/project-form' && session === null) {
-        setModalAuth('login')
-        navigate('/')
-      }
-    })
-  }, [location, navigate])
-
-  return { session, modalAuth, setModalAuth }
 }
