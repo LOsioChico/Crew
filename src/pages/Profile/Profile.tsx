bfb319fe4a478eee7f8c69713b95240006025e9d
VecchioFJ <vecchiofj@gmail.com>
2023-06-23 22:05:02 -0300

    fix: change icon

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,26 +1,36 @@
+import locationPin from '@/assets/locationPin.svg'
+
 export const Profile: React.FC = () => {
   return (
-    <div className='flex h-screen items-center justify-center'>
-      <div className='inline-block w-3/4 items-center justify-center'>
-        <div className='mb-10 mt-16 flex w-full'>
+    <div className='flex justify-center'>
+      <div className='flex w-3/4 flex-col '>
+        <div className='mt-16'>
           <h1 className='text-3xl font-bold'>Your Name</h1>
+        </div>
+        <div className='mb-10 flex items-center'>
+          <img
+            className='mr-2 w-6 text-red-500'
+            src={locationPin}
+            alt='Location Pin'
+          />
+          <h2 className='text-2xl '> País </h2>
         </div>
         <div className='flex items-start justify-start'>
           <div>
-            <p className='text-2xl'>Profile</p>
+            <p className='mr-5 text-2xl'>Profile</p>
           </div>
           <div>
-            <p className='ml-6 text-2xl'>Projects</p>
+            <p className='mr-5 text-2xl'>Projects</p>
           </div>
           <div>
-            <p className='ml-6 text-2xl'>Contributions</p>
+            <p className='mr-5 text-2xl'>Contributions</p>
           </div>
           <div>
-            <p className='ml-6 text-2xl'>Referrals</p>
+            <p className='text-2xl'>Referrals</p>
           </div>
         </div>
-        <hr />
-        <div className='flex h-96 border border-black'>
+        <hr className='border-thick mb-1 border border-black' />
+        <div className='flex h-96 w-full border border-black'>
           <div></div>
           <div></div>
         </div>
