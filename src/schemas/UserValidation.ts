768711f7f67d841f58b2071f6807424b88e75668
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-26 00:12:17 -0500

    style: fix formatting and remove unnecessary comments

--- src/schemas/UserValidation.ts
+++ src/schemas/UserValidation.ts
@@ -1,43 +1,43 @@
 import { z } from 'zod'
-const emailRegex = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
+const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/
 
 export const UserValidation = z.object({
   updateName: z
     .string()
     .min(3, 'El nombre debe tener al menos 3 caracteres')
     .max(15, 'El nombre debe tener menos de 10 caracteres'),
-    updateLastName: z
+  updateLastName: z
     .string()
     .min(3, 'El apellido debe tener al menos 3 caracteres')
     .max(15, 'El apellido debe tener menos de 10 caracteres'),
-    updateEmail: z
+  updateEmail: z
     .string()
     .min(5, 'El correo electrónico debe tener al menos 5 caracteres')
     .max(30, 'El correo electrónico debe tener menos de 30 caracteres')
     .refine((value) => emailRegex.test(value), {
       message: 'Debe proporcionar un correo electrónico válido',
       path: ['email'],
-    }),    
-    updateCountry: z  
+    }),
+  updateCountry: z
     .string()
     .min(3, 'El país debe tener al menos 3 caracteres')
     .max(15, 'El país debe tener menos de 10 caracteres'),
-    updateCity: z  
+  updateCity: z
     .string()
     .min(3, 'La ciudad debe tener al menos 3 caracteres')
     .max(20, 'La ciudad debe tener menos de 10 caracteres'),
-    updateAboutMe: z
+  updateAboutMe: z
     .string()
     .min(10, 'La descripción debe tener al menos 10 caracteres')
     .max(100, 'La descripción debe tener menos de 100 caracteres'),
-    updateShortDescription: z
+  updateShortDescription: z
     .string()
     .min(3, 'La descripción corta debe tener al menos 3 caracteres')
     .max(100, 'La descripción corta debe tener menos de 40 caracteres'),
 })
 
 export type UserSettingsFormType = z.infer<typeof UserValidation>
-
 
 // Para confirmar con un mail enviado
 
